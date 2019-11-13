2a63db22e2c502b18cb6d518786d9380c0de8069
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-11-13 14:55:06 -0800

    mvp

---
+++ users/users-helpers.js
@@ -0,0 +1,18 @@
+module.exports = {
+  validateUser
+};
+
+function validateUser(user){
+  let errors = [];
+
+  if(!user.username || user.username.length < 3){
+    errors.push('please include a username with at least 3 characters ');
+  }
+  if(!user.password || user.password.length < 4){
+    errors.push('please include a password with at least 4 characters')
+  }
+  return {
+    isSuccessful: errors.length > 0 ? false : true,
+    errors
+  };
+}
